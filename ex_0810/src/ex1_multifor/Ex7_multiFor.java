830ff3af3af8fca83d51dc2be40699e12c196531
pakso0708 <pakso0708@gmail.com>
2026-08-10 17:46:22 +0900

    0810 수업내용

--- ex_0810/src/ex1_multifor/Ex7_multiFor.java
+++ ex_0810/src/ex1_multifor/Ex7_multiFor.java
@@ -1,21 +1,24 @@
 package ex1_multifor;
 
-public class Ex7_nultiFor {
+public class Ex7_multiFor {
     public static void main(String[] args) {
         
 
-        // 2 x 2=4 3 x 2=6 4 x 2 =8
-        // 2 x 3= 6 3 x 3 = 9
-        //....
-        // ...
-        //2 x 9= 18 3 x 9= 27...
+        // 2 x 2 = 4 3 x 2 = 6 4 x 2 = 8
+        // 2 x 3 = 6 3 x 3 = 9 4 x 3 = 12 
+        // 2 x 4 = 8 3 x 4 = 12 4 x 4 = 16
+        // 2 x 5 = 10 3 x 5 = 15 4 x 5 = 20
+        // 2 x 6 = 12 3 x 6 = 18 4 x 6 = 24
+        // 2 x 7 = 14 3 x 7 = 21 4 x 7 = 28
+        // 2 x 8 = 16 3 x 8 = 24 4 x 8 = 32
+        // 2 x 9 = 18 3 x 9= 27 4 x 9 = 36
 
 
         for (int i=2;i<=9; i++){
             
             for(int j=2;j<=9; j ++){
         
-                System.out.println(i + " x " + j + " = " + (i * j));
+                System.out.printf( "%d :j, 'x' , i ,(j*i)");
                 
                 
         
--- ex_0810/src/ex1_multifor/Ex8_multiFor.java
+++ ex_0810/src/ex1_multifor/Ex8_multiFor.java
@@ -1,6 +1,6 @@
 package ex1_multifor;
 
-public class Ex8_nultiFor {
+public class Ex8_multiFor {
     public static void main(String[] args) {
         
 
@@ -11,9 +11,22 @@ public class Ex8_nultiFor {
         // ....
         //10 1 2 3 4 5 6 7 8 9 
 
+        for(int i = 1;i<=10;i++ ){
+            for(int j=1; j<=10;j++){
+System.out.printf("%d ",j);
+
+
+
+
+
+        
+        }
+        System.out.println();
+    }
 
 
 
 
 
     }
+}
